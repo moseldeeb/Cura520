df440da41f51d8958485a650daae19248bfa67db
mshabaan12 <mshabaan1231@outlook.com>
2026-03-07 08:30:06 +0200

    mm

---
+++ Cura520/ViewModel/DoctorVM.cs
@@ -0,0 +1,10 @@
+﻿ using Cura520.Models  ;
+namespace Cura520.ViewModel
+{
+    public class DoctorVM
+    {
+        public List<Doctor> Doctors { get; set; }
+        public string Specialty { get; set; } 
+        public ApplicationUser ApplicationUser { get; set; } 
+    }
+}
